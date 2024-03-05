d85e6852ba2985f729cd0d1b5f5ece44b45e8577
Leszek Manicki <leszek.manicki@wikimedia.de>
2024-03-05 21:48:23 +0100

    Do not export submitter ID to CSV files
    Submitter is always the jenkins-bot

--- wikimedia-gerrit/presentation/gerrit_change_list_csv_printer.py
+++ wikimedia-gerrit/presentation/gerrit_change_list_csv_printer.py
@@ -4,7 +4,7 @@ from gerrit_change.gerrit_change import GerritChange
 
 class GerritChangeListCsvPrinter:
     def print(self, changes):
-        fieldnames = ['change_id', 'project', 'branch', 'status', 'subject', 'created_at', 'owner_id', 'submitted_at', 'submitter_id', 'phabricator_refs', 'revision_count', 'files_changed']
+        fieldnames = ['change_id', 'project', 'branch', 'status', 'subject', 'created_at', 'owner_id', 'submitted_at', 'phabricator_refs', 'revision_count', 'files_changed']
         writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
 
         writer.writeheader()
@@ -21,7 +21,6 @@ class GerritChangeListCsvPrinter:
             'created_at': str(change.created_at or ''),
             'owner_id': str(change.owner_id or ''),
             'submitted_at': str(change.submitted_at or ''),
-            'submitter_id': str(change.submitter_id or ''),
             'phabricator_refs': '' if change.phabricator_refs is None else ', '.join(change.phabricator_refs),
             'revision_count': str(change.revision_count or ''),
             'files_changed': '' if change.files_changed is None else '\n'.join(change.files_changed)
