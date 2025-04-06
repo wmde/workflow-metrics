33d558e832072f1e21e10446454681116613e1c9
Leszek Manicki <leszek.manicki@wikimedia.de>
2025-04-06 23:39:29 +0200

    Handle github changes and print them in CSV exports

--- change/change_factory.py
+++ change/change_factory.py
@@ -1,4 +1,6 @@
 from change.change import Change
+from github.github_change.github_change import GithubChange
+
 
 class ChangeFactory:
     def __init__(self, author_affiliation_mapper):
@@ -28,6 +30,34 @@ class ChangeFactory:
             'author_id': author_id,
             'author_organisation': author_org,
             'author_team': author_team,
+        }
+
+        return Change(**change_info)
+
+    def create_from_github_change(self, github_change: GithubChange):
+        author_id = 'github-' + str(github_change.author_id)
+
+        author_org = ''
+        author_team = ''
+        affilitation_data = self.author_affiliation_mapper.get_affiliation_for_author(author_id, github_change.merged_at)
+
+        if not affilitation_data:
+            author_org = 'UNKNOWN:' + author_id
+        elif 'organisation' in affilitation_data:
+            author_org = affilitation_data['organisation']
+
+        if affilitation_data and 'team' in affilitation_data:
+            author_team = affilitation_data['team']
+
+        change_info = {
+            'id': 'github-' + github_change.pull_request_id,
+            'created_at': github_change.created_at,
+            'accepted_at': github_change.merged_at,
+            'repository': github_change.repository,
+            'author_id': author_id,
+            'author_organisation': author_org,
+            'author_team': author_team,
+            'vcs_system': 'github',
         }
 
         return Change(**change_info)
--- print_change_data_as_csv.py
+++ print_change_data_as_csv.py
@@ -3,6 +3,8 @@ import sys
 from change.change_factory import ChangeFactory
 from change.change_csv_printer import ChangeCsvPrinter
 from author.affiliation_mapper import AffiliationMapper
+from github.github_change.github_change_list_json_loader import GithubChangeListJsonLoader
+from github.github_timestamp_converter import GithubTimestampConverter
 from lead_time.business_days_aware_lead_time_calculator import BusinessDaysAwareLeadTimeCalculator
 from lead_time.working_hours_aware_lead_time_calculator import WorkingHoursAwareLeadTimeCalculator
 from wikimedia_gerrit.gerrit_change.gerrit_change_list_json_loader import GerritChangeListJsonLoader
@@ -11,13 +13,19 @@ from lead_time.lead_time_calculator import LeadTimeCalculator
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('json_filename')
+arg_parser.add_argument('--input-type', choices=['gerrit', 'github'], default='gerrit')
 arg_parser.add_argument('--no-header', action='store_true')
 args = arg_parser.parse_args()
 
 json_filename = args.json_filename
+input_type = args.input_type
 no_header = args.no_header
 
-json_loader = GerritChangeListJsonLoader(GerritTimestampConverter())
+json_loader = None
+if input_type == 'gerrit':
+    json_loader = GerritChangeListJsonLoader(GerritTimestampConverter())
+elif input_type == 'github':
+    json_loader = GithubChangeListJsonLoader(GithubTimestampConverter())
 
 author_affiliation_mapper = AffiliationMapper('gerrit_account_data_annotated.csv')
 change_factory = ChangeFactory(author_affiliation_mapper)
@@ -30,13 +38,19 @@ change_csv_printer = ChangeCsvPrinter(sys.stdout)
 if not no_header:
     change_csv_printer.print_header()
 
+changes = []
 with open(json_filename) as file:
     for line in file:
-        gerrit_changes = json_loader.load_change_data_from_json(line.rstrip())
+        system_specific_changes = json_loader.load_change_data_from_json(line.rstrip())
 
-        for gerrit_change in gerrit_changes:
-            change = change_factory.create_from_gerrit_change(gerrit_change)
-            lead_time_in_seconds = lead_time_calculator.get_lead_time(change)
-            business_days_lead_time_in_seconds = business_days_aware_lead_time_calculate.get_lead_time(change)
-            working_hours_lead_time_in_seconds = working_hours_aware_lead_time_calculator.get_lead_time(change)
-            change_csv_printer.print(change, lead_time_in_seconds, business_days_lead_time_in_seconds,working_hours_lead_time_in_seconds)
+        for change_to_process in system_specific_changes:
+            if input_type == 'gerrit':
+                changes.append( change_factory.create_from_gerrit_change(change_to_process) )
+            elif input_type == 'github':
+                changes.append( change_factory.create_from_github_change(change_to_process) )
+
+for change in changes:
+    lead_time_in_seconds = lead_time_calculator.get_lead_time(change)
+    business_days_lead_time_in_seconds = business_days_aware_lead_time_calculate.get_lead_time(change)
+    working_hours_lead_time_in_seconds = working_hours_aware_lead_time_calculator.get_lead_time(change)
+    change_csv_printer.print(change, lead_time_in_seconds, business_days_lead_time_in_seconds,working_hours_lead_time_in_seconds)
