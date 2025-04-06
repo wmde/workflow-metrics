be5948a13e5aee1d527cdf5e345735f12e0cd40e
Leszek Manicki <leszek.manicki@wikimedia.de>
2025-04-06 23:38:47 +0200

    Handle github accounts in author affiliation data CSV

--- author/affiliation_mapper.py
+++ author/affiliation_mapper.py
@@ -5,6 +5,7 @@ class AffiliationMapper:
     def __init__(self, affiliation_data_fielname):
         self.affiliation_by_author = {}
         self.affiliation_by_author_time_bound = {}
+        self.gerrit_account_id_by_github_username = {}
 
         self.load_affiliation_data(affiliation_data_fielname)
 
@@ -13,14 +14,28 @@ class AffiliationMapper:
             reader = csv.DictReader(csv_file)
 
             for row in reader:
-                if not row['wikimedia_gerrit_account_id'] or not row['affiliation']:
+                if not row['wikimedia_gerrit_account_id'] and not row['github_account_name']:
+                    continue
+                if not row['affiliation']:
                     continue
 
                 affiliation_data = {'organisation': row['affiliation']}
                 if row['affiliation_detailed']:
                     affiliation_data['team'] = row['affiliation_detailed']
 
-                id = 'wikimedia-gerrit-' + row['wikimedia_gerrit_account_id']
+                id = None
+                if row['wikimedia_gerrit_account_id']:
+                    id = 'wikimedia-gerrit-' + row['wikimedia_gerrit_account_id']
+                elif row['github_account_name']:
+                    id = 'github-' + row['github_account_name']
+
+                if row['wikimedia_gerrit_account_id'] and row['github_account_name']:
+                    github_author_id = 'github-' + row['github_account_name']
+                    if not github_author_id in self.gerrit_account_id_by_github_username:
+                        self.gerrit_account_id_by_github_username[github_author_id] = id
+
+                if not id:
+                    continue
 
                 if row['affiliation_start'] or row['affiliation_end']:
                     start = None
@@ -37,6 +52,9 @@ class AffiliationMapper:
                 self.affiliation_by_author[id] = affiliation_data
 
     def get_affiliation_for_author(self, author_id, change_time):
+        if author_id in self.gerrit_account_id_by_github_username:
+            author_id = self.gerrit_account_id_by_github_username[author_id]
+
         if author_id in self.affiliation_by_author_time_bound:
             for time_period in self.affiliation_by_author_time_bound[author_id]:
                 matching_period_found = True
